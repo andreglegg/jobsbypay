da0f79dd8482438391ad82466857a12b692be88e
andreglegg <andreglegg@me.com>
2018-03-19 14:38:04 +0100

    Added top nav link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import {
+    NavLink,
+    HashRouter
+} from "react-router-dom";
 import classes from './Navigation.css'
 
 const navigation = () => {
     return (
-        <nav className={classes.Navigation}>
-            <div className={classes.Wrapper}>
-            Logo
-            </div>
-        </nav>
+        <HashRouter>
+            <nav className={classes.Navigation}>
+                <div className={classes.Wrapper}>
+                    <nav>
+                        <NavLink to="/">Logo</NavLink>
+                    </nav>
+                </div>
+            </nav>
+        </HashRouter>
     );
 };
 
